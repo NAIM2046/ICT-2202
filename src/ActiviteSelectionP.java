80f8cd8e3081fb7ccd435ca0a32e93820f799b47
NAIM Naim HOSSAIN <153206925+NAIM2046@users.noreply.github.com>
2025-03-12 11:46:36 +0600

    Update ActiviteSelectionP.java

--- src/ActiviteSelectionP.java
+++ src/ActiviteSelectionP.java
@@ -16,46 +16,46 @@ public class ActiviteSelectionP {
     }
 
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        // int a = 10;
-        // System.out.println(a);
-        // int b[] = { 1, 2, 3, 4, 5, };
-        // for (int i = 0; i < b.length; i++) {
-        // System.out.println(b[i]);
-        // }
-        // int[] nums = new int[5];
+        System.out.println("Hello, World!");
+        int a = 10;
+        System.out.println(a);
+        int b[] = { 1, 2, 3, 4, 5, };
+        for (int i = 0; i < b.length; i++) {
+        System.out.println(b[i]);
+        }
+        int[] nums = new int[5];
         Scanner s = new Scanner(System.in);
-        // for (int i = 0; i < nums.length; i++) {
-        // nums[i] = s.nextInt();nai
-        // }
-        // for (int i = 0; i < nums.length; i++) {
-        // System.out.println(nums[i]);
-        // }
-        // String[] ss = new String[4];
-        // for (int i = 0; i < ss.length; i++) {
-        // ss[i] = s.next();
-        // }
-        // for (String name : ss) {
-        // System.out.println(name);
-        // }
-        // int[][] arr = new int[2][2];
-        // for (int i = 0; i < 2; i++) {
-        // for (int j = 0; j < 2; j++) {
-        // arr[i][j] = s.nextInt();
-        // }
-        // }
-        // for (int i = 0; i < 2; i++) {
-        // for (int j = 0; j < 2; j++) {
-        // System.out.print(arr[i][j] + " ");
-        // }
-        // System.out.println();
-        // }
+        for (int i = 0; i < nums.length; i++) {
+        nums[i] = s.nextInt();nai
+        }
+        for (int i = 0; i < nums.length; i++) {
+        System.out.println(nums[i]);
+        }
+        String[] ss = new String[4];
+        for (int i = 0; i < ss.length; i++) {
+        ss[i] = s.next();
+        }
+        for (String name : ss) {
+        System.out.println(name);
+        }
+        int[][] arr = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+        arr[i][j] = s.nextInt();
+        }
+        }
+        for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+        System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+        }
 
-        // int[] star = { 1, 3, 0, 5, 8, 5 };
-        // int[] end = { 2, 4, 6, 7, 9, 9 };
-        // String[] workName = { "A3", "A2", "A1", "A5", "A6", "A4" };
-        // int n = end.length;
-        // ActiviteSelectionProblem(workName, star, end, n);
+        int[] star = { 1, 3, 0, 5, 8, 5 };
+        int[] end = { 2, 4, 6, 7, 9, 9 };
+        String[] workName = { "A3", "A2", "A1", "A5", "A6", "A4" };
+        int n = end.length;
+        ActiviteSelectionProblem(workName, star, end, n);
         LinkedList<String> c = new LinkedList<String>();
         c.add("omar");
         c.add("kobir");
